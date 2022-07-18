f60b006d115b2e17951bedfc5a120b4ab66da7bd
zackverham <96081108+zackverham@users.noreply.github.com>
2022-07-18 11:27:14 -0400

    getting LSP support up and running

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,8 +19,8 @@ vim.o.hlsearch = false
 -- Make line numbers default
 vim.wo.number = true
 
--- Enable mouse mode
-vim.o.mouse = 'a'
+-- (Dis)able mouse mode
+vim.o.mouse = ''
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -43,6 +43,17 @@ vim.wo.signcolumn = 'yes'
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+
+-- Custom hotkeys
+-- Split-pane
+vim.keymap.set('n', '<Leader>v', ':vsplit<CR>')
+
+-- Moving between panes
+vim.keymap.set('n', '<C-h>', '<C-W>h')
+vim.keymap.set('n', '<C-j>', '<C-W>j')
+vim.keymap.set('n', '<C-k>', '<C-W>k')
+vim.keymap.set('n', '<C-l>', '<C-W>l')
+
 -- plugins
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
@@ -54,11 +65,11 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 local ok, packer = pcall(require, "packer")
-if not ok then	        
+if not ok then
   return
 end
 
-packer.init({		  
+packer.init({
   display = {
     open_fn = function()
       return require('packer.util').float()
@@ -72,7 +83,7 @@ packer.startup(function(use)
 
   -- Parsing library
   use 'nvim-treesitter/nvim-treesitter'
-  
+
   -- Language server protocol: ide-like behavior
   use 'neovim/nvim-lspconfig'
   use 'williamboman/nvim-lsp-installer'
@@ -83,14 +94,14 @@ packer.startup(function(use)
   -- Atom-like theme
   use 'joshdick/onedark.vim'
 
-  -- Git decorations 
+  -- Git decorations
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
 
   -- Heuristically set shiftwidth / expandtab parameters for buffer
-  use 'tpope/vim-sleuth' 
+  use 'tpope/vim-sleuth'
 
   -- Autocomplete
-  use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } }               
+  use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } }
 
   -- Search
   use {
@@ -123,7 +134,7 @@ packer.startup(function(use)
   end
 end)
 
-if is_boostrap then 
+if is_bootstrap then
   print 'Plugins being installed. Wait for Packer to complete, then restart nvim.'
   return
 end
@@ -183,7 +194,7 @@ require('gitsigns').setup {
 
 
 require('nvim-treesitter.configs').setup {
--- Add languages to be installed here that you want installed for treesitter
+  -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'lua', 'javascript', 'go', 'python' },
   highlight = { enable = true },
   indent = { enable = true },
@@ -192,8 +203,62 @@ require('nvim-treesitter.configs').setup {
 
 -- Enable the following language servers
 local servers = { 'pyright', 'sumneko_lua', 'gopls' }
+local ok, _ = pcall(require, 'lspconfig')
+if not ok then
+  return
+end
 
-require('nvim-lsp-installer').setup {
-  ensure_installed = servers,
-}
 
+local function lsp_keymap(bufnr)
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+end
+
+local function lsp_format(client, bufnr)
+  if client.resolved_capabilities.document_formatting then
+    vim.b.format = 1
+    local au = vim.api.nvim_create_augroup('lsp_doc_formatting', { clear = true })
+    vim.api.nvim_create_autocmd({ 'InsertLeave', 'BufWritePre' }, {
+      group = au,
+      buffer = bufnr,
+      callback = function()
+        if vim.b.format == 1 then
+          vim.lsp.buf.formatting()
+        end
+      end
+    })
+  end
+end
+
+local function lsp_on_attach(client, bufnr)
+  lsp_keymap(bufnr)
+  lsp_format(client, bufnr)
+end
+
+local ok, installer = pcall(require, 'nvim-lsp-installer')
+if not ok then
+  return
+end
+
+local client_capabilities = vim.lsp.protocol.make_client_capabilities()
+
+local ok, cmp_lsp = pcall(require, 'cmp_nvim_lsp')
+if ok then
+  client_capabilities = cmp_lsp.update_capabilities(client_capabilities)
+end
+
+installer.on_server_ready(function(server)
+  local opts = {
+    on_attach = lsp_on_attach,
+    capabilitities = client_capabilities
+  }
+
+  local ok, server_opts = pcall(require, "user.lsp.settings." .. server.name)
+  if ok then
+    opts = vim.tbl_deep_extend("force", server_opts, opts)
+  end
+
+  server:setup(opts)
+end)
